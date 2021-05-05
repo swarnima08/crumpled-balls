b0f7d93630b2bf535f1b6c2fcec97d58c4d0467c
swarnima08 <77873878+swarnima08@users.noreply.github.com>
2021-05-05 12:48:00 +0530

    Add files via upload

---
+++ project-c25-template-main/project-c25-template-main/paper.js
@@ -0,0 +1,15 @@
+class paper{
+    constructor(x,y,w,h){
+        this.image=loadImage("paper.png")
+        this.body.position.x = mouseX;
+    this.body.position.y = mouseY;
+    }
+ varoptions={
+    isStatic:false,
+    restitution:0.3,
+    friction:0,
+    density:1.2
+}
+
+
+}
